4ddc7e52ce6f2320f8e7e0dfd3e76884573c80dd
Rasha Rahman <rasharahman@rashas-mbp.lan>
2020-11-13 20:22:11 -0800

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import {theme, darkTheme} from '../src/theme';
+import Favicon from 'react-favicon';
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
@@ -18,6 +19,7 @@ export default function MyApp(props) {
 
   return (
     <React.Fragment>
+      <Favicon url={`https://scontent-lax3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/123184309_644051536278684_8887277192931406801_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com&_nc_cat=100&_nc_ohc=gh5MD2J79YEAX9rxBtX&_nc_tp=24&oh=5740837feccc95dee9b1c1f3ee64722f&oe=5FDA095E`} />
       <Head>
         <title>Rasha Rahman</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
